a83bbe334032d082d96b398a48faf40e49584cdd
Tai Nguyen Thanh <tai.nguyen206k23@hcmut.edu.vn>
2025-10-04 10:57:14 +0700

    Backend/cv (#9)
    * StoringCVfile
    
    * chore: change log messages for codebase consistency
    
    * fix: change file name for db parsing
    
    * fix: add file removal if cannot insert to job_seekers table
    
    * Add cvs table update + GET endpoint
    
    - Add the update to `cvs` table when a cv is uploaded
    - Implement the GET endpoint for viewing CVs data
    
    ---------
    
    Co-authored-by: Tyler <atunguye25@gmail.com>

--- backend/src/routes/cv/cvRoutes.ts
+++ backend/src/routes/cv/cvRoutes.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import { uploadCv, getCv } from "../../controllers/cv/cvController";
+import { authenticateToken } from "../../middleware/auth/jwtAuth";
 
 const router = express.Router();
 
-router.post("/upload", uploadCv);
-router.get("/:id", getCv);
+// Apply authentication middleware to protect the upload route
+router.post("/upload", authenticateToken, uploadCv);
+router.get("/:id", authenticateToken, getCv);
 
 export default router;
